e6661ec8d426a7cd4dd8d34fdf456894692e8303
KIAND-glitch <kiand@student.unimelb.edu.au>
2022-04-22 22:19:27 +1000

    tied to implement post request to add BCG

--- controllers/clinicianController.js
+++ controllers/clinicianController.js
@@ -1,4 +1,12 @@
 const {Patient} = require('../models/patient')
+const {Measurement} = require('../models/patient')
+const {CurrentMeasurement} = require('../models/patient')
+
+const m = new Measurement({
+    type: "BCG",
+    value: 404,
+    comment: "today i recorded 404 error ",
+  });
 
 const getAllPeopleData = async (req, res, next) => {
     try {
@@ -22,14 +30,39 @@ const getDataById = async(req, res, next) => {
     }
 }
 
+const getBloodGlucoseMeasurement = async(req,res,next) => {
+
+    //find pat
+    let thisUser = await Patient.findOne( {first_name: 'John'})
+
+    //find BCG reading
+    let BCGMeasurement = await Measurement.findOne({type: req.params.type})
+    // BCGReading = new CurrentMeasurement({measurementId: BCGMeasurement._id})
+    // thisUser.measurements = thisUser.measurements || [];
+    thisUser.measurements.push(BCGMeasurement)
+
+    await thisUser.save()
+    result = await Patient.findOne( {nameGiven: 'John'})
+    res.send(result)
+}
+
 const insertData = async (req, res, next) => {
     try {
         const newPatient = new Patient({
             first_name: req.body.first_name,
             last_name: req.body.last_name,
             age: req.body.age,
-            join_date: req.body.join_date
-            })
+            join_date: req.body.join_date,
+            recordBCG: req.body.recordBCG,
+            recordWeight: req.body.recordWeight,
+            recordInsulin: req.body.recordInsulin,
+            recordExercise: req.body.recordExercise,
+            measurement:{
+                type: "BCG",
+                value: 404,
+                comment: "today i recorded 404 error ",
+              },
+        })
         await newPatient.save();
         return res.redirect('/clinician')
     }catch (err) {
@@ -40,5 +73,6 @@ const insertData = async (req, res, next) => {
 module.exports = {
     getAllPeopleData,
     getDataById,
-    insertData
+    insertData,
+    getBloodGlucoseMeasurement
 }
--- models/patient.js
+++ models/patient.js
@@ -7,14 +7,23 @@ const measurementSchema = new mongoose.Schema({
     comment: String
 })
 
+const currentMeasurementSchema = new mongoose.Schema({
+    measurementId: {type: mongoose.Schema.Types.ObjectId, ref: 'Measurement'}
+})
+
 const patientSchema = new mongoose.Schema({
     first_name: { type: String, required: true },
     last_name: String,
     age: Number,
     join_date: {type: Date, default: Date.now},
-    measurement:[measurementSchema]
+    recordBCG: {type: Boolean, required:true, default:false},
+    recordWeight: {type: Boolean, required:true, default:false},
+    recordInsulin: {type: Boolean, required:true, default:false},
+    recordExercise: {type: Boolean, required:true, default:false},
+    measurements: [currentMeasurementSchema]
 })
 const Patient = mongoose.model('Patient', patientSchema)
 const Measurement = mongoose.model('Measurement', measurementSchema)
+const CurrentMeasurement = mongoose.model('CurrentMeasurement',currentMeasurementSchema)
 
-module.exports = {Patient, Measurement}
+module.exports = {Patient, Measurement,CurrentMeasurement}
