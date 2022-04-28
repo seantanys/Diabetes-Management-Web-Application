846c3b4cee772b26659560ead9094e59467546da
seantanys <79890362+seantanys@users.noreply.github.com>
2022-04-28 14:48:00 +1000

    Update preventScroll.js

--- public/scripts/preventScroll.js
+++ public/scripts/preventScroll.js
@@ -1,19 +1,5 @@
 // this script prevents user from scrolling when mobile menu is expanded.
 
-// window.onload = function () {
-//   const menuToggle = document.getElementById("menuToggleCheckbox");
-//   const hamborger = document.querySelector('input');
-
-//   menuToggle.addEventListener('change', e => {
-//       if(e.target.checked === true) {
-//         document.body.classList.add("fixed-position")
-//       }
-//     if(e.target.checked === false) {
-//       document.body.classList.remove("fixed-position")
-//       }
-//   });
-// }
-
 window.addEventListener('DOMContentLoaded', (event) => {
   const menuToggle = document.getElementById("menuToggleCheckbox");
 
