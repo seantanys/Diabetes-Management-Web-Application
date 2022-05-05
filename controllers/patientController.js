b7dde98a9e9065f806b8ff3b5fbc318656d55eb7
seantanys <79890362+seantanys@users.noreply.github.com>
2022-05-05 22:52:49 +1000

    auth error fix

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -7,33 +7,39 @@ const { DateTime } = require("luxon");
 // const id = "62660737332717bb9fe3eb55"; 
 
 const getMeasurementPage = async (req, res) => {
-    // get logged in user id
-    const user = req.user 
-    // get current melbourne time using luxon
-    const currTime = DateTime.now().setZone('Australia/Melbourne');
-    // get the beginning of the the current day
-    const currDate = currTime.startOf('day').toISO();
-    const displayTime = currTime.toLocaleString(DateTime.DATETIME_MED)
-    // get the patient's data
-    const data = await Patient.findById(user._id).lean();
-    // get the patient's recorded data today
-    const todayData = await Measurement.find({patientId: user._id, date: { $gte: currDate}}).lean();
 
-    // get the patients required measurements.
-    const reqMeasurements = Object.keys(data["measurements"]) 
-    // get the measurments that have already been recorded for today
-    const alreadyMeasured = getMeasurementTypes(todayData); 
-    // get the measurements that havent been recorded today
-    const notMeasured = reqMeasurements.filter(x => !alreadyMeasured.includes(x)); 
+    if (req.isAuthenticated()) {
+        // get logged in user id
+        const user = req.user 
+        // get current melbourne time using luxon
+        const currTime = DateTime.now().setZone('Australia/Melbourne');
+        // get the beginning of the the current day
+        const currDate = currTime.startOf('day').toISO();
+        const displayTime = currTime.toLocaleString(DateTime.DATETIME_MED)
+        // get the patient's data
+        const data = await Patient.findById(user._id).lean();
+        // get the patient's recorded data today
+        const todayData = await Measurement.find({patientId: user._id, date: { $gte: currDate}}).lean();
 
-    if (data) {
-        res.render('record.hbs', {loggedIn: req.isAuthenticated(), flash: req.flash('success'), singlePatient: data, measured: alreadyMeasured, 
-                                  notMeasured: notMeasured, required: reqMeasurements,
-                                  currentTime: displayTime})
-    } else {
-        console.log("patient data not found")
-        res.render('notfound')
+        // get the patients required measurements.
+        const reqMeasurements = Object.keys(data["measurements"]) 
+        // get the measurments that have already been recorded for today
+        const alreadyMeasured = getMeasurementTypes(todayData); 
+        // get the measurements that havent been recorded today
+        const notMeasured = reqMeasurements.filter(x => !alreadyMeasured.includes(x)); 
+
+        if (data) {
+            res.render('record.hbs', {loggedIn: req.isAuthenticated(), flash: req.flash('success'), singlePatient: data, measured: alreadyMeasured, 
+                                    notMeasured: notMeasured, required: reqMeasurements,
+                                    currentTime: displayTime})
+        } else {
+            console.log("patient data not found")
+            res.render('notfound')
+        }
     }
+    else {
+        res.render('login');
+    }   
 }
 
 // helper function to determine the type of measurement (bcg, insulin, etc.)
@@ -47,55 +53,66 @@ function getMeasurementTypes(arr) {
 
 // this function instantiates a new measurement object and saves it to the db
 const submitMeasurement = async (req, res, next) => {
-    const id = req.user._id
-    try {
-        const newMeasurement = new Measurement ({
-            type: req.body.type,
-            patientId: id,
-            value: parseFloat(req.body.value),
-            date: DateTime.now().setZone('Australia/Melbourne').toISO(),
-            comment: req.body.comment,
-        })
-        await newMeasurement.save();
-        // console.log("Measurement successfully saved to db")
-        if (req.body.type === "bcg") {
-            req.flash('success', "blood glucose level successfully recorded.")
+    if (req.isAuthenticated()) {
+        const id = req.user._id
+        try {
+            const newMeasurement = new Measurement ({
+                type: req.body.type,
+                patientId: id,
+                value: parseFloat(req.body.value),
+                date: DateTime.now().setZone('Australia/Melbourne').toISO(),
+                comment: req.body.comment,
+            })
+            await newMeasurement.save();
+            // console.log("Measurement successfully saved to db")
+            if (req.body.type === "bcg") {
+                req.flash('success', "blood glucose level successfully recorded.")
+            }
+            else {
+                req.flash('success', `${req.body.type} successfully recorded.`)
+            }
+            
+            res.redirect('/patient/record');
+        } catch (err) {
+            next(err);
         }
-        else {
-            req.flash('success', `${req.body.type} successfully recorded.`)
-        }
-        
-        res.redirect('/patient/record');
-    } catch (err) {
-        next(err);
+    }
+    else {
+        res.render('login');
     }
 }
 
 // this function renders the patient dashboard page
 const getPatientPage = async (req, res) => {
-    const user = req.user 
-    const currTime = DateTime.now().setZone('Australia/Melbourne'); 
-    const currDate = currTime.startOf('day').toISO();
-    // get the patient's data
-    const data = await Patient.findById(user._id).lean(); 
-    // get the patient's recorded data today
-    const todayData = await Measurement.find({patientId: user._id, date: { $gte: currDate}}).lean(); 
 
-    // get the patients required measurements.
-    const reqMeasurements = Object.keys(data["measurements"])
-    // get the measurments that have already been recorded for today 
-    const alreadyMeasured = getMeasurementTypes(todayData); 
-    // get the measurements that havent been recorded today
-    const notMeasured = reqMeasurements.filter(x => !alreadyMeasured.includes(x));
-    // format the date for presentation
-    const dob = data.dob.getDate().toString().padStart(2,"0") + "/" + 
-        (data.dob.getMonth() + 1).toString().padStart(2,"0") + "/" + data.dob.getFullYear().toString() 
+    if (req.isAuthenticated()) {
+        const user = req.user 
+        const currTime = DateTime.now().setZone('Australia/Melbourne'); 
+        const currDate = currTime.startOf('day').toISO();
+        // get the patient's data
+        const data = await Patient.findById(user._id).lean(); 
+        // get the patient's recorded data today
+        const todayData = await Measurement.find({patientId: user._id, date: { $gte: currDate}}).lean(); 
 
-    if (data) {
-        res.render('patientDashboard.hbs', {loggedIn: req.isAuthenticated(), dob, singlePatient: data, measured: alreadyMeasured, notMeasured: notMeasured, required: reqMeasurements})
-    } else {
-        console.log("patient data not found")
-        res.render('notfound')
+        // get the patients required measurements.
+        const reqMeasurements = Object.keys(data["measurements"])
+        // get the measurments that have already been recorded for today 
+        const alreadyMeasured = getMeasurementTypes(todayData); 
+        // get the measurements that havent been recorded today
+        const notMeasured = reqMeasurements.filter(x => !alreadyMeasured.includes(x));
+        // format the date for presentation
+        const dob = data.dob.getDate().toString().padStart(2,"0") + "/" + 
+            (data.dob.getMonth() + 1).toString().padStart(2,"0") + "/" + data.dob.getFullYear().toString() 
+
+        if (data) {
+            res.render('patientDashboard.hbs', {loggedIn: req.isAuthenticated(), dob, singlePatient: data, measured: alreadyMeasured, notMeasured: notMeasured, required: reqMeasurements})
+        } else {
+            console.log("patient data not found")
+            res.render('notfound')
+        }
+    }
+    else {
+        res.render('login')
     }
 }
 
