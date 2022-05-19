c916e2839e6374ba6694555d8737a37f094ad572
panhpham <101153325+panhpham@users.noreply.github.com>
2022-05-19 22:11:02 +1000

    Update clinicianRouter.js

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -14,15 +14,30 @@ clinicianRouter.get('/create', clinicianController.getNewPatientForm)
 
 clinicianRouter.post('/create', clinicianController.insertData)
 
-clinicianRouter.get('/messages', clinicianController.getPatientMessages)
+//clinicianRouter.get('/messages', clinicianController.getPatientMessages)
 
-clinicianRouter.get('/supportMessage', clinicianController.changeSupportMessage)
+//clinicianRouter.get('/supportMessage', clinicianController.changeSupportMessage)
 
 // route to handle the GET request for all patients data
 clinicianRouter.get('/dashboard', clinicianController.getAllPatientData)
 
 // route to handle the GET request for one patient data
-clinicianRouter.get('/:patient_id', clinicianController.getDataById)
+clinicianRouter.get('/:patient_id', app.hasRole('clinician'), clinicianController.getPatientOverview) //Anh
+clinicianRouter.post('/:patient_id', app.hasRole('clinician'), clinicianController.writeNote) //Anh
+
+clinicianRouter.get('/:patient_id/bcg', app.hasRole('clinician'), clinicianController.getPatientBCG) //Anh
+clinicianRouter.get('/:patient_id/weight', app.hasRole('clinician'), clinicianController.getPatientWeight) //Anh
+clinicianRouter.get('/:patient_id/insulin', app.hasRole('clinician'), clinicianController.getPatientInsulin) //Anh
+clinicianRouter.get('/:patient_id/exercise', app.hasRole('clinician'), clinicianController.getPatientExercise) //Anh
+
+// Manage patient tab
+clinicianRouter.get('/:patient_id/manage', app.hasRole('clinician'), clinicianController.getDataBoudnds) // Sanskar
+clinicianRouter.post('/:patient_id/manage', app.hasRole('clinician'), clinicianController.manageDataBounds) // Sanskar
+
+// Support messages tab - not sure what Sean is doing for this but this is Anh's format
+//clinicianRouter.get('/:patient_id/message', app.hasRole('clinician'), clinicianController.getSupportMessages) // TO IMPLEMENTTTTTT
+//clinicianRouter.post('/:patient_id/message', app.hasRole('clinician'), clinicianController.writeSupportMessage) // TO IMPLEMENTTTTTT
+
 
 
 
