ffcb78a4432d45230231e5ed9284b7bab24c4c83
Sanskar Agarwal <sanskara@student.unimelb.edu.au>
2022-05-19 21:40:17 +1000

    revert to old route

--- routes/clinicianRouter.js
+++ routes/clinicianRouter.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = require('../app.js');
+// const {Clinician} = require('../models/clinician')
+// const {User} = require('../models/user')
 
 // create our Router object
 const clinicianRouter = express.Router()
@@ -8,34 +10,53 @@ const clinicianRouter = express.Router()
 const clinicianController = require('../controllers/clinicianController')
 
 // route to handle the GET request for creating new patient
-clinicianRouter.get('/create', app.hasRole('clinician'), clinicianController.getNewPatientForm)
+clinicianRouter.get('/create', clinicianController.getNewPatientForm)
 
-clinicianRouter.post('/create', app.hasRole('clinician'), clinicianController.insertData)
+clinicianRouter.post('/create', clinicianController.insertData)
+
+clinicianRouter.get('/messages', clinicianController.getPatientMessages)
+
+clinicianRouter.get('/supportMessage', clinicianController.changeSupportMessage)
 
 // route to handle the GET request for all patients data
-clinicianRouter.get('/dashboard', app.hasRole('clinician'), clinicianController.getAllPatientData)
-
-// route to make note for patient
-clinicianRouter.post('/dashboard', app.hasRole('clinician'), clinicianController.writeNote) // TO IMPLEMENTTTTTT
+clinicianRouter.get('/dashboard', clinicianController.getAllPatientData)
 
 // route to handle the GET request for one patient data
-clinicianRouter.get('/:patient_id', app.hasRole('clinician'), clinicianController.getPatientOverview) 
-clinicianRouter.get('/:patient_id/bcg', app.hasRole('clinician'), clinicianController.getPatientBCG) 
-clinicianRouter.get('/:patient_id/weight', app.hasRole('clinician'), clinicianController.getPatientWeight) 
-clinicianRouter.get('/:patient_id/insulin', app.hasRole('clinician'), clinicianController.getPatientInsulin) 
-clinicianRouter.get('/:patient_id/exercise', app.hasRole('clinician'), clinicianController.getPatientExercise) 
+clinicianRouter.get('/:patient_id', clinicianController.getDataById)
 
-// Manage patient tab
-clinicianRouter.get('/:patient_id/manage', app.hasRole('clinician'), clinicianController.getDataBoudnds) // TO IMPLEMENTTTTTT
-clinicianRouter.post('/:patient_id/manage', app.hasRole('clinician'), clinicianController.manageDataBounds) // TO IMPLEMENTTTTTT
-
-// Support messages tab
-clinicianRouter.get('/:patient_id/message', app.hasRole('clinician'), clinicianController.getSupportMessages) // TO IMPLEMENTTTTTT
-clinicianRouter.post('/:patient_id/message', app.hasRole('clinician'), clinicianController.writeSupportMessage) // TO IMPLEMENTTTTTT
 
 
 // route to handle the POST request new patient, adding to the database
 // clinicianRouter.post('/dashboard', clinicianController.insertData)
 
 // export the router
+
+
+// clinicianRouter.post('/addClinician', async (req, res) => { // using POST for Postman demo
+//     const newClinician = new Clinician({
+//     first_name: "anh@dah.com",
+//     last_name: "pha",
+//     screen_name: "Clinician Anh",
+//     dob: new Date("2001-02-27"),
+//     })
+
+
+//     // extract the object id from the patient document
+//     const clinician = await newClinician.save();  
+//     const clinicianId = clinician._id;
+
+//     // then we create the user document and save to db
+//     const newUser = new User({
+//         username: "anh@dah.com",
+//         password: "anh",
+//         dob: new Date("2001-02-27"),
+//         role: "clinician",
+//         role_id: clinicianId,
+//         theme: "default"
+//     });
+
+//     await newUser.save();
+
+//     })
+
 module.exports = clinicianRouter
