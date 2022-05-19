97e5a06584c02d5c2c01b49777475545389a84d6
seantanys <79890362+seantanys@users.noreply.github.com>
2022-05-20 00:17:33 +1000

    added a button to patient overview in messages

--- public/scripts/supportMessages.js
+++ public/scripts/supportMessages.js
@@ -6,13 +6,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const patientDivs = document.querySelectorAll(".message-patient-contact");
     const messageBubble = document.querySelector('.msg-bubble')
     const currentMsgContainer = document.getElementById('msg-recipient')
+    const mainBody = document.querySelector(".support-message-main");
     
     patientDivs.forEach(patient => {
         patient.addEventListener('click', switchPatient);
     });
 
     function switchPatient() {
+        // remove message
         messageBubble.removeChild(messageBubble.lastChild);
+        
+        // remove previous button to patient overview
+        if (mainBody.childNodes[0].tagName === "A") {
+            mainBody.removeChild(mainBody.childNodes[0]);
+        }
+
+        // change patient displayed name and set tab to active
         var patientName = this.innerText
         toggleActivePatient(patientName);
 
@@ -22,12 +31,21 @@ window.addEventListener('DOMContentLoaded', (event) => {
         var currMsg = document.createElement('p');
         currMsg.innerText = messages[patientName][1];
 
+        // add the button to patient overview
+        var btn = document.createElement('button');
+        var toPatientOverview = document.createElement('a');
+        toPatientOverview.href = `manage-patient/${messages[patientName][0]}`;
+        btn.innerText = "See Patient Overview";
+        btn.classList.add("patient-overview-btn");
+        toPatientOverview.appendChild(btn);
+        mainBody.prepend(toPatientOverview);
+
+        // add message
         currentMsgContainer.innerText = `To ${patientName}`;
         messageBubble.appendChild(currMsg);
     }
 
     function toggleActivePatient(currPatient) {
-
         patientDivs.forEach(patient => {
             if (currPatient !== patient.innerText) {
                 patient.classList.remove("active-message-patient");
